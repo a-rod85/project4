c983bafe79c356cc057bdbcefe5e37495c0b7286
a-rod85 <31aaronanderson@gmail.com>
2022-09-29 13:22:00 +0000

    add system/template folders add book_your_table, cancel_booking, index, login, register, sorry, success and welcome html files

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from system import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+
+
+
+
+
 ]
