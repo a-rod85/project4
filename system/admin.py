937719ec218f39b3c094416990cf3e13b9558d7b
a-rod85 <31aaronanderson@gmail.com>
2022-09-29 13:31:23 +0000

    add template/base.html

--- system/admin.py
+++ system/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Restaurant, Table, Seat, Reservation
 # Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(Table)
+admin.site.register(Seat)
+admin.site.register(Reservation)
